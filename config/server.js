ef188b16198648c86cb9d159eb71031d7d0d0172
RED17 <red17@pop-os.localdomain>
2021-07-29 21:52:35 +0300

    Init repo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '46227dd014b54702696ebe4af89ec13a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24acd4a9-3698-4009-a8ee-a25e9421b3ad'
+};
